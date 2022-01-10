e374bae25e70303de60bcb27131e3807374f56e3
Shoshana Fishman <Shoshana@condotek.com>
2022-01-10 17:38:04 +0200

    test

---
+++ PhaxioTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Phaxio;
+using System;
+using System.IO;
+
+namespace Faxio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var status = SendFax();
+            Console.WriteLine("Status:" + status);
+        }
+
+        public static string SendFax()
+        {
+            string key = "mvq51ug24yh1kxis5isibys6xrrkbswomlf5m1de";
+            string secret = "rxfuoya6txil0ax3eg7fiq5sg2220ptu0snf7xbr";
+
+            var phaxio = new PhaxioClient(key, secret);
+            var fax = phaxio.Fax.Create(toNumbers: new string[] { "215-689-3122" }, contentUrl: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
+            return fax.Status;
+        }
+    }
+}
